24f12fc34fbd1dbd13d9be9a48c8f027660a2624
M. Taufik Nelas <fickysyahreza.fs@gmail.com>
2022-10-22 11:03:42 +0700

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get -y update
 # Install OpenJDK 8
-sudo apt-get -y install openjdk-8-jdk
+sudo apt-get -y install default-jre
 # Install Jenkins
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
